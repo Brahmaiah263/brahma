ddca42c83d43e12a25a26064c764ae2333543f1b
Brahmaiah263 <brahmaiahnagam@gmail.com>
2021-03-23 14:03:01 +0530

    ok

---
+++ vinod/home.js
@@ -0,0 +1,42 @@
+console.log('hello');
+//alert('yoo');
+var b='brahmaiah';
+console.log(b);
+var number=34;
+console.log(number);
+//var age=prompt('What your age?');
+//document.getElementById('demo').innerHTML=age;
+// numbers
+var num=10;
+num+=1;
+console.log(num);
+
+
+//function
+ function fun(){
+    console.log("this is a function");
+}
+fun(); 
+function greeting(uname){
+    
+    var result='hello'+ ' '+uname;
+    console.log(result);
+
+}
+//var name=prompt('what is ur name');
+//greeting(name);
+function sumnumbers(num1 ,num2){
+var result=num1+num2;
+console.log(result);
+}
+sumnumbers('hello',10);
+/*while loop
+var num=0;
+while(num<20){
+    num+=1;
+    console.log(num);
+} */
+//for loop
+for(let num=0;num<=10;num++){
+    console.log(num);
+}
